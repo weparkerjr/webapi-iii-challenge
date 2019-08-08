a3fe2cc5ec1bbc98d25a70de9d7410b907c5021e
william-parker <edwinious@gmail.com>
2019-08-08 09:56:20 -0400

    Initial coommit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code away!
+//load .env variables
+require('dotenv').config();
+
+const server = require('./server.js');
+
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => console.log(`\n*** Server running on port ${port} ***\n`));
